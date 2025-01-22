a87d6c10b3b35433329efca7aa5d960a73809335
Michele Farneti <michele.farneti@studio.unibo.it>
2025-01-23 00:24:43 +0100

    Added nav design

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+//require_once 'bootstrap.php';
+
+//Base Template
+$templateParams["titolo"] = "DBFarm";
+// $templateParams["js"] = array("js/index.js", "js/nav.js", "js/header.js");
+
+//Index template
+
+require 'template/base.php';
+?>
---
+++ template/base.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="it">
+
+<head>
+    <title>DBFarm</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/styles.css">
+</head>
+
+<body>
+    <header>
+        <h1>DBFarm</h1>
+    </header>
+    <nav>
+        <ul>
+            <li class="selected">
+                Lavorazioni
+            </li>
+            <li>
+                Terreni
+            </li>
+            <li>
+                Macchinari
+            </li>
+            <li>
+                Magazzini
+            </li>
+            <li>
+                Silo
+            </li>
+        </ul>
+    </nav>
+</body>
+
+</html>
